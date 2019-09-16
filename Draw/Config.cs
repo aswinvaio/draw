d6300cd5e194b84e22e828846c92358d542e7819
aswin vp <aswinprabhakaran@gmail.com>
2019-09-17 01:18:04 +0530

    initial

---
+++ Draw/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Draw
+{
+    public class Config
+    {
+        public static string Get(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
