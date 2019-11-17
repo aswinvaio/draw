f591d014461ff4bfc4796e7d2d29bf563884a240
aswin vp <aswinprabhakaran@gmail.com>
2019-11-18 02:14:15 +0530

    draw complete

---
+++ DrawDeploy/Java.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawDeploy
+{
+    public class Java
+    {
+        public static bool Do()
+        {
+            string root = Config.Get("java-root");
+            string drawExe = Config.Get("draw-exe");
+            string drawFileName = Config.Get("draw-file-name");
+            string backupFolder = Config.Get("backup-folder");
+            string appExe = Config.Get("java-exe");
+
+            #region backup 
+            if (!Directory.Exists(root))
+            {
+                Console.WriteLine("root not exists");
+                return false;
+            }
+
+            if (!Directory.Exists(string.Format(@"{0}\{1}", root, backupFolder)))
+            {
+                Directory.CreateDirectory(string.Format(@"{0}\{1}", root, backupFolder));
+            }
+
+            if (File.Exists(string.Format(@"{0}\{1}", root, appExe)))
+            {
+                try
+                {
+                    File.Move(string.Format(@"{0}\{1}", root, appExe), string.Format(@"{0}\{1}\{2}", root, backupFolder, appExe));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            if (File.Exists(string.Format(@"{0}\{1}.config", root, appExe)))
+            {
+                try
+                {
+                    File.Move(string.Format(@"{0}\{1}.config", root, appExe), string.Format(@"{0}\{1}\{2}.config", root, backupFolder, appExe));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            #endregion
+
+            if (File.Exists(string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, drawExe))
+                && File.Exists(string.Format(@"{0}\{1}.config", AppDomain.CurrentDomain.BaseDirectory, drawExe))
+                && File.Exists(string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, drawFileName))
+                )
+            {
+                try
+                {
+                    File.Copy(string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, drawExe)
+                        , string.Format(@"{0}\{1}", root, appExe));
+                    File.Copy(string.Format(@"{0}\{1}.config", AppDomain.CurrentDomain.BaseDirectory, drawExe)
+                        , string.Format(@"{0}\{1}.config", root, appExe));
+                    File.Copy(string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, drawFileName)
+                        , string.Format(@"{0}\{1}", root, drawFileName));
+
+                    Console.WriteLine("Success!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+                return false;
+
+            return true;
+        }
+
+        public static bool Undo()
+        {
+            string root = Config.Get("java-root");
+            string drawExe = Config.Get("draw-exe");
+            string drawFileName = Config.Get("draw-file-name");
+            string backupFolder = Config.Get("backup-folder");
+            string appExe = Config.Get("java-exe");
+
+            if (!File.Exists(string.Format(@"{0}\{1}\{2}", root, backupFolder, appExe)))
+            {
+                Console.WriteLine("Java Backup not found");
+                return false;
+            }
+
+            try
+            {
+                File.Delete(string.Format(@"{0}\{1}", root, appExe));
+                File.Move(string.Format(@"{0}\{1}\{2}", root, backupFolder, appExe), string.Format(@"{0}\{1}", root, appExe));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (File.Exists(string.Format(@"{0}\{1}.config", root, appExe)))
+            {
+                try
+                {
+                    File.Delete(string.Format(@"{0}\{1}.config", root, appExe));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            if (File.Exists(string.Format(@"{0}\{1}\{2}.config", root, backupFolder, appExe)))
+            {
+                try
+                {
+                    File.Move(string.Format(@"{0}\{1}\{2}.config", root, backupFolder, appExe), string.Format(@"{0}\{1}.config", root, appExe));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            if (File.Exists(string.Format(@"{0}\{1}", root, drawFileName)))
+            {
+                try
+                {
+                    File.Delete(string.Format(@"{0}\{1}", root, drawFileName));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+
+            return true;
+        }
+    }
+}
--- DrawDeploy/Program.cs
+++ DrawDeploy/Program.cs
@@ -10,6 +10,61 @@ namespace DrawDeploy
     {
         static void Main(string[] args)
         {
+            AppTypes type = AppTypes.Unknown;
+            Operation operation = Operation.Deploy;
+
+            if (args.Length <= 0) return;
+
+            try
+            {
+                switch (args[0].ToLower())
+                {
+                    case "all":
+                        type = AppTypes.All;
+                        break;
+                    case "ng":
+                    case "angular":
+                        type = AppTypes.Ng;
+                        break;
+                    case "java":
+                        type = AppTypes.Java;
+                        break;
+                }
+
+                switch (args[1].ToLower())
+                {
+                    case "deploy":
+                    case "destroy":
+                        operation = Operation.Deploy;
+                        break;
+                    case "rollback":
+                    case "fix":
+                        operation = Operation.Rollback;
+                        break;
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid options");
+                return;
+            }
+
+            if(type == AppTypes.All || type == AppTypes.Ng)
+            {
+                if (operation == Operation.Deploy)
+                    Angular.Do();
+                else
+                    Angular.Undo();
+            }
+            if (type == AppTypes.All || type == AppTypes.Java)
+            {
+                if (operation == Operation.Deploy)
+                    Java.Do();
+                else
+                    Java.Undo();
+            }
+            Console.WriteLine("done");
         }
     }
 }
